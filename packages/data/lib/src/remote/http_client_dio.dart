8b62898eb35c9b9fa9aef68576654826cf056d8c
Daniel Ribas <danielribas@tokenlab.com.br>
2022-10-03 20:27:32 -0300

    feat: implement a wrapper around dio to map errors

---
+++ packages/data/lib/src/remote/http_client_dio.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:dio/native_imp.dart';
+import 'package:domain/domain.dart';
+
+class HttpClientDio extends DioForNative {
+  HttpClientDio([BaseOptions? baseOptions]) : super(baseOptions);
+
+  @override
+  Future<Response<T>> request<T>(
+    String path, {
+    data,
+    Map<String, dynamic>? queryParameters,
+    CancelToken? cancelToken,
+    Options? options,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) =>
+      super
+          .request<T>(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        cancelToken: cancelToken,
+        options: options,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      )
+          .catchError(
+        (e) {
+          mapDioErrorToDomainError(e);
+        },
+        test: (e) => e is Exception,
+      );
+
+  void mapDioErrorToDomainError(Exception e) {
+    if (e is DioError) {
+      if (e.error is SocketException) {
+        throw NoConnectionException(e);
+      }
+
+      final statusCode = e.response?.statusCode;
+      if (statusCode != null) {
+        if (statusCode == 401) {
+          throw UnauthorizedException(e);
+        }
+        if (statusCode == 403) {
+          throw ForbiddenException(e);
+        }
+        if (statusCode >= 400 && statusCode <= 499) {
+          throw BadRequestException(e);
+        }
+        if (statusCode >= 500 && statusCode <= 599) {
+          throw InternalServerException(e);
+        }
+      }
+    }
+
+    throw UnexpectedException(e);
+  }
+}
