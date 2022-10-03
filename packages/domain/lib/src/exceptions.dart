dabecedc64d0789975074e63e187ebe74cf3f2c2
Daniel Ribas <danielribas@tokenlab.com.br>
2022-10-03 19:30:44 -0300

    feat: create domain exceptions

--- packages/domain/lib/src/exceptions.dart
+++ packages/domain/lib/src/exceptions.dart
@@ -1,1 +1,29 @@
-abstract class DomainException implements Exception {}
+abstract class DomainException implements Exception {
+  DomainException(this.baseError);
+
+  final Exception? baseError;
+}
+
+class NoConnectionException extends DomainException {
+  NoConnectionException(super.baseError);
+}
+
+class UnauthorizedException extends DomainException {
+  UnauthorizedException(super.baseError);
+}
+
+class TimeoutException extends DomainException {
+  TimeoutException(super.baseError);
+}
+
+class BadRequestException extends DomainException {
+  BadRequestException(super.baseError);
+}
+
+class InternalServerException extends DomainException {
+  InternalServerException(super.baseError);
+}
+
+class UnexpectedException extends DomainException {
+  UnexpectedException(super.baseError);
+}
